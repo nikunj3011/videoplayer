5b03d38c6c51acbcf5fae7d9c7e51c3e71dea671
Nikunj Rathod <nikunjrathod3011@gmail.com>
2020-01-30 01:40:35 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var playing = false;
+var completion;
+function setup() {
+  createCanvas(400, 100);
+  vid = createVideo("characters2.mp4");
+  vid.size(400, 300);
+}
+function draw() {
+  background(50);
+  completion = vid.time() / vid.duration();
+  ellipse(completion*width, 50, 20, 20);
+}
+function mousePressed() {
+  if (!playing) {
+    vid.play();
+    vid.time((mouseX/width) * vid.duration());
+    playing = true;
+  }
+  else {
+    vid.pause();
+    playing = false;
+  }
+}
